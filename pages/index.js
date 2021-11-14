265aa8a603e1375111159d4f7ca35847e1537324
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-11-14 21:48:49 +0600

    Fetch Products and display on home page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,45 @@
 import Link from 'next/link'
-import Navbar from '../components/navbar'
+import Image from 'next/image'
 
-const Home = (prop) => {
+const Home = ({products}) => {
+  // console.log(products);
+  const productList= products.map(product => (  
+    <div key={product._id} className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+        <div className='relative md:h-20 md:w-20 lg:h-56 lg:w-full'>
+          <Image
+           className="rounded-t-lg" 
+           src={product.mediaUrl}
+          //  width={40}
+          //  height={40}
+           layout="fill"
+          //  objectFit="cover"
+           alt={product.name}/>
+        </div>
+        <div className="p-5">
+            <a href="#">
+                <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">{product.name}</h5>
+            </a>
+            <p className="font-normal text-gray-700 mb-3">${product.price}</p>
+            <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center">
+                Read more
+                <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            </a>
+        </div>
+    </div>
+  ))
   return (
-    <div>
-      <h1 className='text-2xl font-bold'>Next js is awesome</h1>
-      <h2 className="text-xl font-semibold">{prop.message}</h2>
-      <Link href='/product'><a className='text-purple-900 underline'>Go to Product Page</a></Link>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-11/12 mx-auto items-center">
+      {productList}
     </div>
   )
 }
 
 // We use getSTatic props when it is not user specific (i.e. a welcome screen which is same for all users. getServerSide is user specific, for example, going to a cart whihc is unique to each user)
-export async function getStaticProps(context) { // only runs in server side
-  const res = await fetch('http://localhost:3000/api/test');
+export async function getStaticProps() { // only runs in server side
+  const res = await fetch('http://localhost:3000/api/products');
   const data = await res.json();
-  console.log(data); // this is why this only logs in server side and not in client side's browser
   return {
-    props: {message:data.message}, // will be passed to the page component as props
+    props: {products:data}, // will be passed to the page component as props
   }
 }
 
